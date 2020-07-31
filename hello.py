f04abb09426eb1310e337d25f0150c7551381fe9
doleo <azard@163.com>
2020-07-31 17:07:45 +0800

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,14 @@ print("hello python!\n")
 
 def add(a,b):
     return a+b
+def sub(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def dev(a,b):
+    return a/b
 
 print("a+b = ",add(3,2))
+print("a/b = ",dev(3,2))
