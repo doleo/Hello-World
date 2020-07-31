2a6625c8c8c6c738cf512549858e46d9d7e725de
doleo <azard@163.com>
2020-07-31 16:23:28 +0800

    modified:   hello.py 	new file:   world_hello.php 	deleted:    hello_world.php

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("hello world!\n")
 print("hello python!\n")
+
+def add(a,b):
+    return a+b
+
+print("a+b = ",add(3,2))
---
+++ world_hello.php
@@ -0,0 +1,5 @@
+<?php
+echo "Hello World!\n"
+echo "Welcome to git world!"
+
+?>
